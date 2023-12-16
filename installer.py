816693fba54d0885a659dd3066f65c5a7bb17f7c
kaelzzzz <152340023+kaelzzzz@users.noreply.github.com>
2023-12-16 15:43:37 +0800

    Add files via upload

---
+++ installer.py
@@ -0,0 +1,40 @@
+import os
+
+print("""
+NUDOS VIP!
+""")
+
+print("""[y] npm installer""")
+rn = input(">>>")
+if rn == 'y':
+    os.system("npm install url")
+    os.system("npm install fs")
+    os.system("npm i user-agents")
+    os.system("npm install http2")
+    os.system("npm install http")
+    os.system("npm install tls")
+    os.system("npm install crypto")
+    os.system("npm install request")
+    os.system("npm install cluster")
+    os.system("npm install fake-useragent")
+    os.system("npm install randomstring")
+    os.system("npm install child_process")
+    os.system("npm install events")
+    os.system("npm install net")
+    os.system("npm install axios")
+    os.system("npm install cheerio")
+    os.system("npm install os")
+    os.system("npm install colors")
+    os.system("npm install minimist")
+    os.system("npm install cloudscraper")
+    os.system("npm install header-generator")
+    os.system("npm install gradient-string")
+    os.system("npm install path")
+    os.system("apt upgrade -y")
+    os.system("apt install screen -y")
+    os.system("apt update -y")
+    
+
+    
+    print("FINISH!")
+
